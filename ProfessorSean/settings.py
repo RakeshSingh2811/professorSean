a871b95bb932be6d3a7265c3d1d8d70d8078ed7d
RakeshSingh2811 <128033460+Rakesh-Es-dev@users.noreply.github.com>
2023-09-29 18:58:30 +0530

    hosting settings

--- ProfessorSean/settings.py
+++ ProfessorSean/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 if 'CODESPACE_NAME' in os.environ:
     CSRF_TRUSTED_ORIGINS = [
