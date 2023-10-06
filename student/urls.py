9fe4f48d68d2b4c032dad2ac1d7c1dfba84ef868
RakeshSingh2811 <128033460+Rakesh-Es-dev@users.noreply.github.com>
2023-10-06 13:17:20 +0530

    adding url in homepage and changes urls.py

--- student/urls.py
+++ student/urls.py
@@ -3,11 +3,12 @@ from django.contrib.auth import views as auth_views
 from . import views
 urlpatterns = [
     path('', auth_views.LoginView.as_view(
-        template_name="student/login.html"), name="login"),
-    path('login/', auth_views.LoginView.as_view(template_name="student/login.html"), name="login"),
-    path('logout/', auth_views.LogoutView.as_view(), name="logout"),
-    path('signup/', views.Signup.as_view(), name="signup"),
-    path("course/", views.Courses.as_view(), name="course"),
+        template_name="student/login.html"), name="studentlogin"),
+    path('login/', auth_views.LoginView.as_view(template_name="student/login.html"),
+         name="studentlogin"),
+    path('logout/', auth_views.LogoutView.as_view(), name="studentlogout"),
+    path('signup/', views.Signup.as_view(), name="studentsignup"),
+    path("course/", views.Courses.as_view(), name="studentcourse"),
     path("courseDetails/<course_id>",
          views.CourseDetails.as_view(), name="courseDetails"),
     path("topicDetails/<course_id>/chapter/<chapter_id>/topic/<topic_id>",
