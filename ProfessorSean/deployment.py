735ba967a1f78731a4309f4b6776e8dba8edb4d7
RakeshSingh2811 <128033460+Rakesh-Es-dev@users.noreply.github.com>
2023-10-06 17:13:40 +0530

    changes in deployment.py

--- ProfessorSean/deployment.py
+++ ProfessorSean/deployment.py
@@ -40,3 +40,32 @@ STATICFILES_DIRS = [
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 SCM_DO_BUILD_DURING_DEPLOYMENT = True
+
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'filters': {
+        'require_debug_false': {
+            '()': 'django.utils.log.RequireDebugFalse'
+        }
+    },
+
+    'handlers': {
+        'mail_admins': {
+            'level': 'ERROR',
+            'filters': ['require_debug_false'],
+            'class': 'django.utils.log.AdminEmailHandler'
+        },
+        'console': {
+            'level': 'DEBUG',
+            'class': 'logging.StreamHandler',
+        },
+    },
+    'loggers': {
+        'django.request': {
+            'handlers': ['mail_admins'],
+            'level': 'ERROR',
+            'propagate': True,
+        },
+    }
+}
