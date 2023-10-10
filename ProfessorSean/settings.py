5fbde404dd5bbd6225089001eb23a41758b1a5e9
RakeshSingh2811 <128033460+Rakesh-Es-dev@users.noreply.github.com>
2023-10-10 17:27:30 +0530

    replace / with \\ in media root

--- ProfessorSean/settings.py
+++ ProfessorSean/settings.py
@@ -118,8 +118,8 @@ STATICFILES_DIRS = [
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
-MEDIA_URL = "./media/"
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media').replace('\\', '/')
+MEDIA_URL = '/media/'
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
