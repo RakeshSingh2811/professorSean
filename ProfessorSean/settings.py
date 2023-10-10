b167914593b1edf871a872093932125baf1df83b
RakeshSingh2811 <128033460+Rakesh-Es-dev@users.noreply.github.com>
2023-10-10 17:11:00 +0530

    adding ./ before media url

--- ProfessorSean/settings.py
+++ ProfessorSean/settings.py
@@ -119,7 +119,7 @@ STATICFILES_DIRS = [
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
-MEDIA_URL = "media/"
+MEDIA_URL = "./media/"
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
