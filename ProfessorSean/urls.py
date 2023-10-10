56ccf26820e3da8f2a90081fc2f95503a946fd6f
RakeshSingh2811 <128033460+Rakesh-Es-dev@users.noreply.github.com>
2023-10-10 18:16:42 +0530

    debugging media files

--- ProfessorSean/urls.py
+++ ProfessorSean/urls.py
@@ -26,5 +26,6 @@ urlpatterns = [
     path("student/", include('student.urls')),
     path("admin/", admin.site.urls),
     path('tinymce/', include('tinymce.urls')),
+    path('media/<path>', serve, {'document_root': settings.MEDIA_ROOT}),
     path('static/<path>', serve, {'document_root': settings.STATIC_ROOT}),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT) + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
